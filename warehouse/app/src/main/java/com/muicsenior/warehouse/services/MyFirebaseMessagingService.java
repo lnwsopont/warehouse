f584dcffe622677879ac60d9d947efad88a41cbd
sopon trangansri <xs>
2017-11-24 09:01:29 +0700

    no message

---
+++ warehouse/app/src/main/java/com/muicsenior/warehouse/services/MyFirebaseMessagingService.java
@@ -0,0 +1,105 @@
+package com.muicsenior.warehouse.services;
+
+/**
+ * Created by Ta on 2017-08-27.
+ */
+
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.muicsenior.warehouse.R;
+import com.muicsenior.warehouse.activities.MainActivity;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Map;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        // TODO(developer): Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+        RemoteMessage.Notification notification = remoteMessage.getNotification();
+        Map<String, String> data = remoteMessage.getData();
+
+        String msg1 = notification.getTitle();
+        String msg2 = notification.getBody();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("title", msg1);
+        intent.putExtra("body", msg2);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        startActivity(intent);
+
+        sendNotification(notification, data);
+    }
+
+    /**
+     * Create and show a custom notification containing the received FCM message.
+     *
+     * @param notification FCM notification payload received.
+     * @param data         FCM data payload received.
+     */
+    private void sendNotification(RemoteMessage.Notification notification, Map<String, String> data) {
+        Bitmap icon = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setContentTitle(notification.getTitle())
+                .setContentText(notification.getBody())
+                .setAutoCancel(true)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setContentIntent(pendingIntent)
+                .setContentInfo(notification.getTitle())
+                .setLargeIcon(icon)
+                .setColor(Color.RED)
+                .setSmallIcon(R.mipmap.ic_launcher);
+
+        for(String k : data.keySet()){
+            Log.i("aaa", k + ": " + data.get(k));
+        }
+
+        try {
+            String picture_url = data.get("picture_url");
+            if (picture_url != null && !"".equals(picture_url)) {
+                URL url = new URL(picture_url);
+                Bitmap bigPicture = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                notificationBuilder.setStyle(
+                        new NotificationCompat.BigPictureStyle().bigPicture(bigPicture).setSummaryText(notification.getBody())
+                );
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        notificationBuilder.setDefaults(Notification.DEFAULT_VIBRATE);
+        notificationBuilder.setLights(Color.YELLOW, 1000, 300);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
